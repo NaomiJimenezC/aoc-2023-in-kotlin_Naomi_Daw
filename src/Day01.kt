648faef167a2b84a20a71b570bc439a1728f9961
NaomiJimenezC <ijimenezcerpa@iesrafaelalberti.es>
2023-12-01 10:01:40 +0100

    getCalibrationValueOfALine made it

--- src/Day01.kt
+++ src/Day01.kt
@@ -8,10 +8,19 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
+    fun getCalibrationValueOfALine(lineOfText:String):String{
+        val numberInTheLine = lineOfText.filter { it.isDigit() }
+        val firstDigit = numberInTheLine.first()
+        val lastDigit = numberInTheLine.last()
+        val calibrationValue = "$firstDigit$lastDigit"
+        return calibrationValue
+    }
+
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val calibratios_values = mutableListOf<String>()
+
+    println(calibratios_values)
 }
