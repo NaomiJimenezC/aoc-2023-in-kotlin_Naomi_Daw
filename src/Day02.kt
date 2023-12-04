b36b17802e9dc05afd3615ec193b45f391469a0a
NaomiJimenezC <ijimenez@iesrafaelalberti.es>
2023-12-04 16:26:31 +0100

    Day 02 part 2 completed

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,36 +1,59 @@
 import kotlin.reflect.typeOf
 
-fun main() {
-    val partidasJugadas = readInput("Day02")
+fun pairsBalls(partidaDividida: List<String>): MutableList<Pair<Int, String>> {
+    val jugadaPorPartida = partidaDividida[1].replace(";", ",")
+    val jugadasParseadas = mutableListOf<Pair<Int, String>>()
+    for (bolaColor in jugadaPorPartida.split(",")) {
+        var numeroBola = bolaColor.filter { it -> it.isDigit() }.toInt()
+        var colorBola = bolaColor.filter { it -> it.isLetter() }
+        jugadasParseadas.add(Pair(numeroBola, colorBola))
+    }
+    return jugadasParseadas
+}
+
+
+
+fun part1(partidasJugadas: List<String>):Int{
     val numMaxBolasPorColor = mapOf<String,Int?>("red" to 12,"green" to 13, "blue" to 14)
+    var partidasValidas = mutableListOf<Int>()
 
+    for (partida in partidasJugadas) {
 
-    fun part1(input: List<String>):Int{
-        var partidasValidas = mutableListOf<Int>()
+        val partidaDividida = partida.split(":")
 
-        for (partida in partidasJugadas) {
+        val idPartida = partidaDividida[0].filter { it -> it.isDigit() }.toInt()
 
-            val partidaDividida = partida.split(":")
+        val jugadasParseadas = pairsBalls(partidaDividida)
 
-            val idPartida = partidaDividida[0].filter { it -> it.isDigit() }.toInt()
-
-            val jugadaPorPartida = partidaDividida[1].replace(";", ",")
-            val jugadasParseadas = mutableListOf<Pair<Int,String>>()
-            for (bolaColor in jugadaPorPartida.split(",")){
-                var numeroBola = bolaColor.filter { it -> it.isDigit() }.toInt()
-                var colorBola = bolaColor.filter { it -> it.isLetter() }
-                jugadasParseadas.add(Pair(numeroBola,colorBola))
-            }
-
-            if (jugadasParseadas.all { it.first <= (numMaxBolasPorColor[it.second]?.toInt() ?: 0) }){
-                partidasValidas.add(idPartida)
-            }
-
+        if (jugadasParseadas.all { it.first <= (numMaxBolasPorColor[it.second]?.toInt() ?: 0) }){
+            partidasValidas.add(idPartida)
         }
-        return partidasValidas.sum()
 
     }
-    val prueba = part1(partidasJugadas)
-    println(prueba)
+    return partidasValidas.sum()
+}
+
+
+fun part2(partidasJugadas:List<String>):Int{
+    val resultados = mutableListOf<Int>()
+    for (partidaJugada in partidasJugadas){
+        val resultadosPartida = partidaJugada.split(":")
+        val bolasResultado = pairsBalls(resultadosPartida)
+        val resultadosDivididosPorColores = bolasResultado.groupBy({ it.second },{it.first})
+        val cubosMinimosPorJuego = mutableListOf<Int>()
+        resultadosDivididosPorColores.values.forEach { it -> cubosMinimosPorJuego.add(it.max()) }
+        val powerForGame =cubosMinimosPorJuego.reduce { factor1, factor2 -> factor1 * factor2 }
+        resultados.add(powerForGame)
+    }
+    return resultados.sum()
+}
+
+fun main() {
+    val partidasJugadas = readInput("Day02")
+    val parte1 = part1(partidasJugadas)
+    println(parte1)
+
+    val parte2 = part2(partidasJugadas)
+    println(parte2)
 }
 
